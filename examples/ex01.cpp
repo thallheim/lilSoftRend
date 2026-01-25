c685d73dc82d0fc7f1d0edb69a374bf9b87568dc
thall <thall@thallheim.com>
2026-01-25 17:51:53 +0100

    yezz

--- examples/ex01.cpp
+++ examples/ex01.cpp
@@ -14,12 +14,16 @@
 
 int main() {
   using namespace lsr::renderer;
+  using std::print;
+  using std::println;
 
   // xcb_connection_t *connection = xcb_connect (NULL, NULL);
   Renderer r;
-  r._disp_conn = xcb_connect (NULL, NULL);
+  int d;
+  r._disp_conn = xcb_connect (NULL, &d);
 
   // xcb_disconnect(connection);
   xcb_disconnect(r._disp_conn);
+  print("Disp: {}\n", d);
   return 0;
 }
