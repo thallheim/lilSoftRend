cdf706a649a3b81556682eda568c8547c52dbffd
thall <thall@thallheim.com>
2026-01-26 01:12:36 +0100

    hmm

--- include/lilSoftRend.hpp
+++ include/lilSoftRend.hpp
@@ -4,27 +4,27 @@
 #include <X11/Xutil.h>
 #include <X11/keysymdef.h>
 #include <xcb/xcb.h>
+#include <xcb/xproto.h>
 
 #include "../include/windowing.hpp"
 
 namespace lsr::renderer {
 
 struct Renderer {
-  Display           *_display   = NULL;
+  int               _display    = NULL;
   xcb_connection_t  *_disp_conn = NULL;
-  Window            *_window    = NULL;
-  Screen            *_screen    = NULL;
+  xcb_screen_t      *_screen    = NULL;
+  xcb_window_t      *_window    = NULL;
 
   Renderer() {}
 
-  Renderer(Display *d, Window *w, Screen *s)
-    : _display(d), _window(w), _screen(s)
+  Renderer(int disp_num, xcb_window_t *w, xcb_screen_t *s)
+    : _display(disp_num), _window(w), _screen(s)
   {
 
   }
 
   bool Init();
-
   void Connect();
   };
 
