51a1ef177f00af4f2cdbfc52b97eeec568151467
thall <thall@thallheim.com>
2026-01-27 11:23:18 +0100

    Window created

--- examples/ex01.cpp
+++ examples/ex01.cpp
@@ -1,5 +1,6 @@
 #include "../include/lilSoftRend.hpp"
 #include "../include/input.hpp"
+#include <unistd.h>
 
 
 #define W_WIDTH    800
@@ -21,16 +22,32 @@ int main() {
 
   Renderer r;
   int disp_id;
-  r._disp_conn = xcb_connect (NULL, &disp_id);
-
   std::map<std::string, uint32_t> ids;
-  ids.emplace("window", xcb_generate_id(r._disp_conn));
 
-  auto win = xcb_create_window(r._disp_conn, XCB_COPY_FROM_PARENT, ids.at("window"), NULL,
-                    0, 0, 800, 600, 1, XCB_WINDOW_CLASS_COPY_FROM_PARENT,
-                    XCB_COPY_FROM_PARENT, XCB_BACK_PIXMAP_NONE, NULL);
+  r._disp_conn = xcb_connect (NULL, &disp_id);
+  xcb_window_t win = xcb_generate_id(r._disp_conn);
+
+  const xcb_setup_t     *setup = xcb_get_setup(r._disp_conn);
+  xcb_screen_iterator_t siter  = xcb_setup_roots_iterator(setup);
+  xcb_screen_t          *screen = siter.data;
+
+  ids.emplace("window", win);
+
+  xcb_create_window(r._disp_conn,
+                    XCB_COPY_FROM_PARENT,
+                    // ids.at("window"),
+                    win,
+                    screen->root,
+                    0, 0, 800, 600,
+                    10,
+                    XCB_WINDOW_CLASS_INPUT_OUTPUT,
+                    screen->root_visual,
+                    XCB_BACK_PIXMAP_NONE,
+                    NULL);
+
   xcb_map_window(r._disp_conn, win);
-
+  xcb_flush(r._disp_conn);
+  pause();
   xcb_disconnect(r._disp_conn);
 
   print("Disp: {}\n", disp_id);
