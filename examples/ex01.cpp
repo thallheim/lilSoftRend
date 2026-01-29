cc46a3ee3035277eedd368ec07c06afb35374a35
thall <thall@thallheim.com>
2026-01-29 14:13:13 +0100

    Yeh

--- examples/ex01.cpp
+++ examples/ex01.cpp
@@ -13,7 +13,7 @@ int main() {
   Renderer r;
   if (!r.init()) return -1;
   Drawable2d d(50, 50);
-  // framebuf f;
+  framebuf f(800, 600);
   vec3f v(1.0, 2.0, 3.6925);
   std::print("vx: {:+.5f} vy: {:+.5f} vz: {:+.5f}\n", v.x, v.y, v.z);
   r.shutdown();
